ff8ad35b488e3ee638b8bb6450affbebeac74531
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-10-16 09:36:24 -0600

    chore: added comments

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import sklearn
+from keras.src.activations import linear
 from sklearn import linear_model
 from sklearn.utils import shuffle
 import tensorflow
@@ -10,9 +11,19 @@ data = pd.read_csv("student-mat.csv", sep=";")
 
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 
+# predicting the final grade. This is also known as a label.
 predict = "G3"
 
-x = np.array(data.drop([predict], 1))
-y = np.array(data[predict])
 
+# defining the attributes.
+x = np.array(data.drop([predict], 1))
+# defining the labels.
+y = np.array(data[predict])
+# splitting the labels and attributes into 4 variables: "x_train, y_train, x_test, y_test".
+# x_train is a section of x array.
+# y_train is a section of y array.
+# x_test, y_test: these are testing the accuracy of the model we are going to create.
+# The code below is splitting %10 of our data into test samples so when we test we can test off of that and the model has never seen the actual data. (if the model sees the actual data it will memorize it, and we don't want that).
 x_train, y_train, x_test, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
+
+linear = linear_model.LinearRegression()
