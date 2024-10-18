87f60a9588c22cec5bc13b4962b962e8bc8c5e47
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-10-17 19:46:01 -0600

    feat: saving the modal

--- test.py
+++ test.py
@@ -6,6 +6,9 @@ from sklearn import linear_model
 from sklearn.utils import shuffle
 import tensorflow
 import keras
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 
 data = pd.read_csv("student-mat.csv", sep=";")
 
@@ -27,12 +30,18 @@ y = np.array(data[predict])
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
 
 # creating a training model:
-linear = linear_model.LinearRegression()
+"""linear = linear_model.LinearRegression()
 # fitting the data to find the best fit line and store it in "linear".
 linear.fit(x_train, y_train)
 # testing the accuracy of our model.
 acc = linear.score(x_test, y_test)
 print(acc)
+
+with open("studentmodel.pickle", "wb") as f:
+    pickle.dump(linear, f)"""
+
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
 
 print('Coefficient: \n', linear.coef_)
 print('Intercept: \n', linear.intercept_)
