3b7ad1b8c785799eb533a4b72ce33ff63a7d08d8
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2024-11-02 10:11:31 -0600

    feat: training and predecting data using SVM

--- SVM/SVM Working File.py
+++ SVM/SVM Working File.py
@@ -1,17 +1,29 @@
 import sklearn
 from sklearn import datasets
 from sklearn import svm
+from sklearn import metrics
 
 #Importing data
 cancer = datasets.load_breast_cancer()
 
-print(cancer.feature_names)
-print(cancer.target_names)
+# print(cancer.feature_names)
+# print(cancer.target_names)
 
 x = cancer.data
 y = cancer.target
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
 
-print(x_train, y_train)
+# print(x_train, y_train)
 classes = ['malignant' 'benign']
+
+# classifier
+clf = svm.SVC(kernel = "linear")
+clf.fit(x_train, y_train)
+
+y_pred = clf.predict(x_test)
+
+# comparing the two lists
+acc = metrics.accuracy_score(y_test, y_pred)
+
+print(acc)
